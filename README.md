a3ea8e0e79fab458df333eac02b53c7299801b74
ChrisShep98 <shepardchristopher1@gmail.com>
2025-05-29 23:02:34 -0400

    added more plugins, moved plugins to separate folders

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Some scape commands/notes I want to remember:
 
 :Ex - explore (gets you out of file and back to directory tree)
 
-
+"a" when adding a new file/dir to neo-tree
 
 
--- init.lua
+++ init.lua
@@ -1,1 +1,44 @@
-require("config.lazy")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+-- change color of strings
+--vim.api.nvim_create_autocmd("ColorScheme", {
+--  pattern = "*",
+--  callback = function()
+    -- Override string color
+--    vim.api.nvim_set_hl(0, "String", { fg = "#ffd866" })
+--    vim.api.nvim_set_hl(0, "@string", { fg = "#ffd866" })
+--  end,
+-- })
+
+local opts = {}
+
+-- Setup lazy.nvim
+require("lazy").setup("plugins")
+
+-- initalizing telescope below and setting keymaps (might move this to a new file in the future)
+
+
+
+
