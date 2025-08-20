b4b5b9f4ee1856e5cbb30c7208b3d95571d03130
ChrisShep98 <shepardchristopher1@gmail.com>
2025-08-19 22:09:46 -0400

    added to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master',
-    lazy = false, 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
       -- not sure if this is working correctly
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript", "typescript", "tsx", "html", "css"},
         auto_install = true, -- should auto install languages when you open different files
+        ensure_installed = { "lua", "javascript", "typescript", "tsx", "html", "css" },
         highlight = { enable = true },
         indent = { enable = true },
       })
-    end
-  }
+    end,
+  },
 }
