d7346aaf020ca029df6382f3bc9c68a0ee4edf94
ChrisShep98 <shepardchristopher1@gmail.com>
2025-08-22 10:28:38 -0400

    added barbar plugin

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim",  -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
+    config = function()
+      vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+      vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+    end
+  },
+}
+
+
