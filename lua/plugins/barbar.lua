05392411a82a475ede4090fe0f5a35dfb82ccd2a
ChrisShep98 <shepardchristopher1@gmail.com>
2025-11-17 21:01:56 -0500

    added keymap for closing file tabs

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -6,10 +6,9 @@ return {
       "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
     },
     config = function()
-      vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-      vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
-    end
+      vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+      vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+      vim.keymap.set("n", "<A-w>", "<Cmd>BufferClose<CR>", opts )
+    end,
   },
 }
-
-
