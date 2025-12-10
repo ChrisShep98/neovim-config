bc81bf0f48d96d80453b540f78588248d4904655
ChrisShep98 <shepardchristopher1@gmail.com>
2025-12-10 10:42:28 -0500

    new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,24 +9,30 @@ return {
   -- },
   --  },
   {
-  	"projekt0n/github-nvim-theme",
-  	name = "github-theme",
-  	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  	priority = 1000, -- make sure to load this before all the other start plugins
-  	config = function()
-  		require("github-theme").setup({
-  			-- ...
-  			groups = {
-  				all = {
-  					["@string"] = { fg = "#ffd866" }, -- Treesitter string
-  					String = { fg = "#ffd866" }, -- fallback string
-  					NeoTreeGitModified = { fg = "#ffd866" }, -- Neo-tree git modified override
-  				},
-  			},
-  		})
-  		vim.cmd("colorscheme github_dark_default")
-  	end,
-  },
+    "rebelot/kanagawa.nvim",
+    config = function ()
+      vim.cmd("colorscheme kanagawa-wave")
+    end
+  }
+  -- {
+  -- 	"projekt0n/github-nvim-theme",
+  -- 	name = "github-theme",
+  -- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  -- 	priority = 1000, -- make sure to load this before all the other start plugins
+  -- 	config = function()
+  -- 		require("github-theme").setup({
+  -- 			-- ...
+  -- 			groups = {
+  -- 				all = {
+  -- 					["@string"] = { fg = "#ffd866" }, -- Treesitter string
+  -- 					String = { fg = "#ffd866" }, -- fallback string
+  -- 					NeoTreeGitModified = { fg = "#ffd866" }, -- Neo-tree git modified override
+  -- 				},
+  -- 			},
+  -- 		})
+  -- 		vim.cmd("colorscheme github_dark")
+  -- 	end,
+  -- },
   -- {
   -- 	"AlexvZyl/nordic.nvim",
   -- 	lazy = false,
