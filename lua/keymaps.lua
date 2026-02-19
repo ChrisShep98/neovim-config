db231899d6c0afe51fb61d0ff8e035155b4e0eeb
ChrisShep98 <shepardchristopher1@gmail.com>
2026-02-19 16:51:21 -0500

    set to keymap to space w to save file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,4 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
