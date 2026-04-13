abdefc4838b02c22cb7eb35a2fab9625f95e7612
ChrisShep98 <shepardchristopher1@gmail.com>
2026-04-13 17:02:24 -0400

    added keymap to show error diagnostic message

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,4 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error message' })
