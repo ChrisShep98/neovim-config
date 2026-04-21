0cc72f079000ff471ff9431e44e143f71cfc6db8
ChrisShep98 <shepardchristopher1@gmail.com>
2026-04-21 16:42:30 -0400

    lsp rename keymap added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -53,6 +53,7 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set({ "n", "v" }, "<leader>re", vim.lsp.buf.rename, { desc = "rename a word globally" })
     end,
   }
 }
