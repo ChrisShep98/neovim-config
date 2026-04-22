16e1111ea0b2b76a4d21e5925997c6b4dcbd2942
ChrisShep98 <shepardchristopher1@gmail.com>
2026-04-22 13:35:27 -0400

    bash color change

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -22,6 +22,7 @@ return {
             ["@constant.builtin.tsx"] = { fg = "#7aa89f" },
             ["@constant.typescript"] = { fg = theme.fujiWhite },
             ["@constant.tsx"] = { fb = theme.fujiWhite },
+            ["@constant.bash"] = { fb = theme.fujiWhite },
             ["@punctuation.special.tsx"] = { fg = "#9CABCA" },
             -- Semantic Token overrides
             ["@lsp.mod.readonly.typescriptreact"] = { fg = theme.fujiWhite },
