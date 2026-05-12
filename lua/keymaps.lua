85ed69489bb3e41797b42d7a5fa0f76d99727d3f
ChrisShep98 <shepardchristopher1@gmail.com>
2026-05-12 16:04:32 -0400

    added keymap for quitting current buffer and quitting all

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,4 +2,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit current buffer' })
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error message' })
